948e0fb473fbd98cb1045bd48888d87d72b014ab
qi lin <qlin1996@gmail.com>
2021-01-16 17:15:50 -0500

    set up upload route

--- server/api/index.js
+++ server/api/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 module.exports = router;
 
 router.use('/images', require('./images'));
+router.use('/upload', require('./upload'));
 
 router.use((req, res, next) => {
   const error = new Error('Not Found');
--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
+require('dotenv/config');
 const express = require('express');
 const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 5000;
 
 const db = require('./db');
 db.sync()
   .then(() => console.log('Database connected...'))
   .catch((err) => console.log('Error:' + err));
-
-const app = express();
-const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
