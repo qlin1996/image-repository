991f975f5051f4c18adbe3874efc83fd108492ba
qi lin <qlin1996@gmail.com>
2021-01-17 20:54:45 -0500

    added path

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
+const path = require('path');
 
 const db = require('./db');
 db.sync()
